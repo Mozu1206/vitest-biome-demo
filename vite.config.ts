bf65fbdae864b7e5ff19f35e8fb14a98142133ad
Mozu1206 <takahashi.masayuki@hrbrain.co.jp>
2024-12-18 20:00:56 +0900

    Feat: Update vite config for test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+    }
+  }
 })
