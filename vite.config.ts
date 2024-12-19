7b45788f83ca1eb08ae8b16372c940531597aa2d
Mozu1206 <takahashi.masayuki@hrbrain.co.jp>
2024-12-19 18:03:32 +0900

    FIx: Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from "@vitejs/plugin-react-swc";
 /// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
